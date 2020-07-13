d9e500b2665c746f2cb970f5410cee12504e852f
GambitEvans <vini_shotgun01@hotmail.com>
2020-07-13 14:24:20 -0300

    removing unnecessary logs.

--- store-back/user/userController.js
+++ store-back/user/userController.js
@@ -2,8 +2,6 @@ const userService = require('./userService');
 
 async function singIn(req, res) {
     const userData = await userService.singIn(req.body);
-    console.log('chegou no controller.');
-    console.log(userData);
     if(userData) {
         return res.json({
             status: 200, 
@@ -17,14 +15,11 @@ async function singIn(req, res) {
 async function singUp(req, res) {
     try {
         const user = await userService.singUp(req.body);
-        console.log('doideira');
-        console.log(user);
         if(user) {
             return res.json({
                 status: 200, 
                 data: user });
         } else {
-            console.log('User already exists');
             return res.json({
                 status: 400,
                 message: 'User already exists'
